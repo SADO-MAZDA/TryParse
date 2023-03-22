9e986040ed426c708d5daf62abc0579d3b382a96
Александр <@WIN-AL3IMBA9OKE>
2023-03-22 19:35:50 +0300

    Добавьте файлы проекта.

---
+++ TryParse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userStrihg  ;
+            userStrihg = Console.ReadLine();
+            int number;
+            bool results = int.TryParse(userStrihg, out number);
+
+            while(true)
+            {
+                if (results == true)
+                {
+                    Console.WriteLine("Успешно - " + number);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Еще раз");
+                }
+            }
+        }  
+    }
+}
